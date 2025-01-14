51c38f297c3c0776010c23fe50d86456be986863
Balša Bazović <balsa.bazovic@gmail.com>
2025-01-14 17:05:03 +0100

    Updated Helper class code structure.

--- sport-scout/assets/class/Helper.php
+++ sport-scout/assets/class/Helper.php
@@ -2,33 +2,33 @@
 
 class Helper
 {
-    public static function setRoleIdAndName($input, &$status, &$role_id, &$role_name, $key)
+    public static function setRoleIdAndName($input, &$status, &$roleID, &$roleName, $key)
     {
         $role = explode('|', $input[$key]);
 
         if (count($role) < 2) {
             $status = 'fail';
         } else {
-            $role_id = $role[0];
-            $role_name = $role[1];
+            $roleID = $role[0];
+            $roleName = $role[1];
         }
     }
 
-    public static function setSportName($db, $input, &$status, &$sport_id, &$sport_name, $key)
+    public static function setSportName($db, $input, &$status, &$sportID, &$sportName, $key)
     {
-        $sport_id = (int) $input[$key];
-        if ($sport_id === '') {
+        $sportID = (int) $input[$key];
+        if ($sportID === '') {
             $status = 'fail';
-        } else if ($sport_id <= 0) {
+        } else if ($sportID <= 0) {
             $status = '';
-            $sport_id = '';
+            $sportID = '';
         } else {
-            $sports = $db->get_all_sports();
+            $sports = $db->getAllSports();
 
             if (count($sports) > 0) {
                 foreach ($sports as $sport) {
-                    if ((int) $sport['sport_id'] === $sport_id) {
-                        $sport_name = $sport['sport_name'];
+                    if ((int) $sport['sport_id'] === $sportID) {
+                        $sportName = $sport['sport_name'];
                         break;
                     }
                 }
@@ -36,84 +36,84 @@ class Helper
         }
 
         // If sport does not exist.
-        if ($sport_name === '') {
+        if ($sportName === '') {
             $status = 'fail';
-            $sport_id = '';
+            $sportID = '';
         }
     }
 
-    public static function setLeagueAndTeamNames($db, $input, &$status, &$league_id, &$league_name, &$team_id, &$team_name, $key1, $key2)
+    public static function setLeagueAndTeamNames($db, $input, &$status, &$leagueID, &$leagueName, &$teamID, &$teamName, $key1, $key2)
     {
-        $league_id = (int) $input[$key1];
-        if ($league_id === '') {
+        $leagueID = (int) $input[$key1];
+        if ($leagueID === '') {
             $status = 'fail';
-        } else if ($league_id === 0) {
-            $league_name = 'All';
-        } else if ($league_id < 0) {
+        } else if ($leagueID === 0) {
+            $leagueName = 'All';
+        } else if ($leagueID < 0) {
             $status = 'fail';
-            $league_id = '';
+            $leagueID = '';
         } else {
-            $leagues = $db->get_all_leagues();
+            $leagues = $db->getAllLeagues();
 
             if (count($leagues) > 0) {
                 foreach ($leagues as $league) {
-                    if ((int) $league['league_id'] === $league_id) {
-                        $league_name = $league['league_name'];
+                    if ((int) $league['league_id'] === $leagueID) {
+                        $leagueName = $league['league_name'];
                         break;
                     }
                 }
             }
         }
 
-        if ($league_name === '') {
+        if ($leagueName === '') {
             $status = 'fail';
-            $league_id = '';
+            $leagueID = '';
         }
 
-        $team_id = (int) $input[$key2];
-        if ($team_id === '') {
+        $teamID = (int) $input[$key2];
+        if ($teamID === '') {
             $status = 'fail';
-        } else if ($league_id === 0 && $team_id === 0) {
-            $team_name = 'All';
-        } else if ($league_id !== '' && $league_id >= 0 && $team_id === 0) {
-            $team_name = "All Within The {$league_name}";
-        } else if ($team_id < 0) {
+        } else if ($leagueID === 0 && $teamID === 0) {
+            $teamName = 'All';
+        } else if ($leagueID !== '' && $leagueID >= 0 && $teamID === 0) {
+            $teamName = "All Within The {$leagueName}";
+        } else if ($teamID < 0) {
             $status = 'fail';
-            $team_id = '';
+            $teamID = '';
         } else {
-            $teams = $db->get_teams_by_league_id($league_id);
+            $teams = $db->getTeamsByLeagueId($leagueID);
 
             if (count($teams) > 0) {
                 foreach ($teams as $team) {
-                    if ((int) $team['team_id'] === $team_id) {
-                        $team_name = $team['team_name'];
+                    if ((int) $team['team_id'] === $teamID) {
+                        $teamName = $team['team_name'];
                         break;
                     }
                 }
             }
         }
 
-        if ($team_name === '') {
+        if ($teamName === '') {
             $status = 'fail';
-            $team_id = '';
+            $teamID = '';
         }
     }
 
-    public static function setLeagueName($db, $input, &$status, &$sport_id, &$league_id, &$league_name, $key)
+    public static function setLeagueName($db, $input, &$status, &$sportID, &$leagueID, &$leagueName, $key)
     {
-        $league_id = (int) $input[$key];
-        if ($league_id === '') {
+        $leagueID = (int) $input[$key];
+        if ($leagueID === '') {
             $status = 'fail';
-        } else if ($league_id <= 0) {
+        } else if ($leagueID <= 0) {
             $status = 'fail';
-            $league_id = '';
-        } else if ($sport_id > 0) {
-            $leagues = $db->get_leagues_by_sport_id($sport_id);
+            $leagueID = '';
+        } else if ($sportID > 0) {
+            $leagues = $db->getLeaguesBySportId($sportID);
 
             if (count($leagues) > 0) {
                 foreach ($leagues as $league) {
-                    if ((int) $league['league_id'] === $league_id) {
-                        $league_name = $league['league_name'];
+                    if ((int) $league['league_id'] === $leagueID) {
+                        $leagueName = $league['league_name'];
                         break;
                     }
                 }
@@ -121,39 +121,39 @@ class Helper
         }
 
         // If sport does not exist.
-        if ($league_name === '') {
+        if ($leagueName === '') {
             $status = 'fail';
-            $league_id = '';
+            $leagueID = '';
         }
     }
 
-    public static function setTeamName($db, $input, &$status, &$team_id, &$team_name, $key)
+    public static function setTeamName($db, $input, &$status, &$teamID, &$teamName, $key)
     {
-        $team_id = (int) $input[$key];
-        if ($team_id === '') {
+        $teamID = (int) $input[$key];
+        if ($teamID === '') {
             $status = 'fail';
-        } else if ($team_id <= 0) {
+        } else if ($teamID <= 0) {
             $status = 'fail';
-            $team_id = '';
+            $teamID = '';
         } else {
-            $team_data = $db->get_team_data_by_team_id('team_name', $team_id);
+            $teamData = $db->getTeamDataByTeamId('team_name', $teamID);
 
-            if (count($team_data) > 0) {
-                $team_name = $team_data[0]['team_name'];
+            if (count($teamData) > 0) {
+                $teamName = $teamData[0]['team_name'];
             }
         }
 
-        if ($team_name === '') {
+        if ($teamName === '') {
             $status = 'fail';
-            $team_id = '';
+            $teamID = '';
         }
     }
 
-    public static function validateSeason($team_data, &$status, &$team_id)
+    public static function validateSeason($teamData, &$status, &$teamID)
     {
         $is_valid = false;
-        foreach ($team_data as $team) {
-            if ($team_id === $team['team_id']) {
+        foreach ($teamData as $team) {
+            if ($teamID === $team['team_id']) {
                 $is_valid = true;
                 break;
             }
@@ -161,11 +161,11 @@ class Helper
 
         if (!$is_valid) {
             $status = 'fail';
-            $team_id = '';
+            $teamID = '';
         }
     }
 
-    public static function setSeasonYear($db, $input, &$status, &$league_id, &$season_id, &$season_year, $key)
+    public static function setSeasonYear($db, $input, &$status, &$leagueID, &$season_id, &$seasonYear, $key)
     {
         $season_id = (int) $input[$key];
         if ($season_id === '') {
@@ -174,12 +174,12 @@ class Helper
             $status = 'fail';
             $season_id = '';
         } else if ($season_id > 0) {
-            $seasons = $db->get_seasons_by_league_id($league_id);
+            $seasons = $db->getSeasonsByLeagueId($leagueID);
 
             if (count($seasons) > 0) {
                 foreach ($seasons as $season) {
                     if ((int) $season['season_id'] === $season_id) {
-                        $season_year = $season['season_year'];
+                        $seasonYear = $season['season_year'];
                         break;
                     }
                 }
@@ -187,36 +187,36 @@ class Helper
         }
 
         // If sport does not exist.
-        if ($season_year === '') {
+        if ($seasonYear === '') {
             $status = 'fail';
             $season_id = '';
         }
     }
 
-    public static function setPositionName($db, $input, &$status, &$sport_id, &$position_id, &$position_name, $key)
+    public static function setPositionName($db, $input, &$status, &$sportID, &$positionID, &$positionName, $key)
     {
-        $position_id = (int) $input[$key];
-        if ($position_id === '') {
+        $positionID = (int) $input[$key];
+        if ($positionID === '') {
             $status = 'fail';
-        } else if ($position_id <= 0) {
+        } else if ($positionID <= 0) {
             $status = 'fail';
-            $position_id = '';
+            $positionID = '';
         } else {
-            $positions = $db->get_positions_by_sport_id($sport_id);
+            $positions = $db->getPositionsBySportId($sportID);
 
             if (count($positions) > 0) {
                 foreach ($positions as $position) {
-                    if ((int) $position['position_id'] === $position_id) {
-                        $position_name = $position['position_name'];
+                    if ((int) $position['position_id'] === $positionID) {
+                        $positionName = $position['position_name'];
                         break;
                     }
                 }
             }
         }
 
-        if ($position_name === '') {
+        if ($positionName === '') {
             $status = 'fail';
-            $position_id = '';
+            $positionID = '';
         }
     }
 }
